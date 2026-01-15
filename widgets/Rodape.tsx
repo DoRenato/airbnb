159cc418927671646bf3046179c1cbbe0c509fe0
DoRenato <64191958+DoRenato@users.noreply.github.com>
2026-01-15 08:14:36 -0300

    Update Rodape.tsx

--- widgets/Rodape.tsx
+++ widgets/Rodape.tsx
@@ -5,16 +5,22 @@ import Link from "next/link";
 
 export default function Rodape() {
   return (
-    <div className="container flex mx-auto items-center pt-10 pb-25 gap-1">
-        <span className="text-md">
+    <ul className="container flex mx-auto items-center pt-10 pb-25 gap-1">
+        <li className="text-md">
             &copy; 2026 Airbnb, Inc. 
-        </span>
-        <div className="font-bold">&#183;</div>
-        <Link href={"/"} className="text-md px-2 hover:underline hover:decoration-2">Privacidade</Link>
-        <div className="font-bold">&#183;</div>
-        <Link href={"/"} className="text-md px-2 hover:underline hover:decoration-2">Termos</Link>
-        <div className="font-bold">&#183;</div>
-        <Link href={"/"} className="text-md px-2 hover:underline hover:decoration-2">Informações da empresa</Link>
-    </div>
+        </li>
+        <li className="font-bold">&#183;</li>
+        <li>
+          <Link href={"/"} className="text-md px-2 hover:underline hover:decoration-2">Privacidade</Link>
+        </li>
+        <li className="font-bold">&#183;</li>
+        <li>
+          <Link href={"/"} className="text-md px-2 hover:underline hover:decoration-2">Termos</Link>
+        </li>
+        <li className="font-bold">&#183;</li>
+        <li>
+          <Link href={"/"} className="text-md px-2 hover:underline hover:decoration-2">Informações da empresa</Link>
+        </li>
+    </ul>
   );
 }
